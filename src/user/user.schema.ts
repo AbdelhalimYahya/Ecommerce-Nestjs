a39bb0de97c0626cd75f30dd0c1faa9d5cb4673c
Abdelhalim Yahya <www.quran0987654321@gmail.com>
2026-01-11 16:51:17 +0200

    initialize the user schema

--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -5,14 +5,62 @@ export type UserDocument = HydratedDocument<User>;
 
 @Schema({ timestamps: true })
 export class User {
-  @Prop()
+  @Prop({
+    required: true,
+    type: String,
+    min:[3, 'Name must be at least 3 characters'], 
+    max:[20, 'Name must be at most 20 characters']
+  })
   name: string;
 
-  @Prop()
-  age: number;
+  @Prop({
+    required: true,
+    unique: true,
+    type: String
+  })
+  email: string;
 
-  @Prop()
-  breed: string;
+  @Prop({
+    required: true,
+    type: String,
+    min:[6, 'Password must be at least 6 characters'],
+    max:[20, 'Password must be at most 20 characters']
+  })
+  password: string;
+
+  @Prop({
+    required: true,
+    type: String,
+    enum: ['user' , 'admin']
+  })
+  role: string;
+
+  @Prop({type: String})
+  avatar: string;
+
+  @Prop({type:Number})
+  age :number;
+
+  @Prop({type:String})
+  phoneNumber: string;
+
+  @Prop({type:String})
+  address: string;
+
+  @Prop({
+    type: String,
+    enum:['false' , 'true']
+  })
+  active: string
+
+  @Prop({type:String})
+  varificationCode: string;
+
+  @Prop({
+    type:String,
+    enum:['male' , 'female']
+  })
+  gender: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
